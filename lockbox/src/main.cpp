0a3e9a29ae44647df163449147a6c3591139f359
S. Santos <ssantosj21@gmail.com>
2024-11-09 00:56:46 -0300

    Add CL11 lib

--- lockbox/src/main.cpp
+++ lockbox/src/main.cpp
@@ -1,7 +1,14 @@
 #include <crow.h>
 #include "library.h"
+#include "CLI11.hpp"
 
-int main() {
+int main(int argc, char *argv[]) {
+
+    CLI::App cli_app{"Lockbox Server"};
+    cli_app.set_version_flag("--version", std::string("0.0.1"));
+
+    CLI11_PARSE(cli_app, argc, argv);
+
     // Initialize Crow HTTP server
     crow::SimpleApp app;
 
