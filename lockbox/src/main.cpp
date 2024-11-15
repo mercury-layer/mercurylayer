03aff19132211ba977e33ecacedd4a113a38790a
S. Santos <ssantosj21@gmail.com>
2024-11-15 01:58:40 -0300

    Add Cloud Secret Manager

--- lockbox/src/main.cpp
+++ lockbox/src/main.cpp
@@ -4,14 +4,14 @@
 #include <toml++/toml.h>
 
 #include "google/cloud/kms/v1/key_management_client.h"
+#include "google/cloud/secretmanager/v1/secret_manager_client.h"
 #include "google/cloud/location.h"
 #include <iostream>
 
-int main(int argc, char *argv[]) {
-
+void test_kms() {
     try {
         auto location_id = "global";
-        auto project_id = "";
+        auto project_id = "mercury-441416";
 
         auto const location = google::cloud::Location(project_id, location_id);
         std::cout << "Location: " << location.FullName() << std::endl;
@@ -24,13 +24,13 @@ int main(int argc, char *argv[]) {
         //     std::cout << kr->DebugString() << "\n";
         // }
 
-        auto key_ring_name =  location.FullName() + "/keyRings/";
+        auto key_ring_name =  location.FullName() + "/keyRings/enclave";
         auto kr = client.GetKeyRing(key_ring_name);
         std::cout << kr->DebugString() << "\n";
 
         std::cout << "kr name: " << kr->name() << "\n";
 
-        auto crypto_key_name = kr->name() + "/cryptoKeys/";
+        auto crypto_key_name = kr->name() + "/cryptoKeys/sealing";
         std::cout << "ck crypto_key_name: " << crypto_key_name << "\n";
 
         auto ck = client.GetCryptoKey(crypto_key_name);
@@ -46,6 +46,30 @@ int main(int argc, char *argv[]) {
     } catch (google::cloud::Status const& status) {
         std::cerr << "google::cloud::Status thrown: " << status << "\n";
     }
+}
+
+void test_secret() {
+
+    auto project_id = "mercury-441416";
+    namespace secretmanager = ::google::cloud::secretmanager_v1;
+    auto client = secretmanager::SecretManagerServiceClient(
+        secretmanager::MakeSecretManagerServiceConnection());
+
+    auto const parent = std::string("projects/") + project_id;
+    for (auto secret : client.ListSecrets(parent)) {
+        if (!secret) throw std::move(secret).status();
+        std::cout << secret->DebugString() << "\n";
+    }
+
+
+}
+
+
+int main(int argc, char *argv[]) {
+
+    test_kms();
+
+    test_secret();
 
     CLI::App cli_app{"Lockbox Server"};
     cli_app.set_version_flag("--version", std::string("0.0.1"));
