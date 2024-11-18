ea96de1287d861b74fee7f1a0d46d58b03ce0b71
S. Santos <ssantosj21@gmail.com>
2024-11-18 18:38:47 -0300

    [WIP] Add Cloud Secret Manager

--- lockbox/src/main.cpp
+++ lockbox/src/main.cpp
@@ -53,6 +53,7 @@ void test_secret() {
     auto project_id = "mercury-441416";
     auto project_number = "100600525477";
     auto key_name = "key";
+    auto version = 1;
 
     namespace secretmanager = ::google::cloud::secretmanager_v1;
     auto client = secretmanager::SecretManagerServiceClient(
@@ -70,15 +71,69 @@ void test_secret() {
     if (!secret) throw std::move(secret).status();
     std::cout << secret->DebugString() << "\n";
 
-    
+    auto secret_version_name = secret_name + "/versions/" + std::to_string(version);
+    auto secret_version = client.AccessSecretVersion(secret_version_name);
+    if (!secret_version) throw std::move(secret_version).status();
+    // std::cout << secret_version->DebugString() << "\n";
+    std::cout << "secret_version name: " << secret_version->name() << "\n";
+    std::cout << "secret_version payload: " << secret_version->payload().data() << "\n";
 }
 
+std::string get_encrypted_secret() {
+    auto project_id = "mercury-441416";
+    auto project_number = "100600525477";
+    auto key_name = "encrypted-key";
+    auto version = 1;
+
+    namespace secretmanager = ::google::cloud::secretmanager_v1;
+    auto client = secretmanager::SecretManagerServiceClient(
+        secretmanager::MakeSecretManagerServiceConnection());
+
+    auto const parent = std::string("projects/") + project_number;
+    auto secret_name = parent + "/secrets/" + key_name;
+    auto secret_version_name = secret_name + "/versions/" + std::to_string(version);
+
+    auto secret_version = client.AccessSecretVersion(secret_version_name);
+    if (!secret_version) throw std::move(secret_version).status();
+
+    return secret_version->payload().data();
+}
+
+std::string decrypt_secret(std::string const& encrypted_secret) {
+    auto location_id = "global";
+    auto project_id = "mercury-441416";
+    auto key_ring = "enclave";
+    auto crypto_key = "sealing";
+
+    namespace kms = ::google::cloud::kms_v1;
+    auto client = kms::KeyManagementServiceClient(kms::MakeKeyManagementServiceConnection());
+
+    auto const location = google::cloud::Location(project_id, location_id);
+    auto crypto_key_name =  location.FullName() + "/keyRings/" + key_ring + "/cryptoKeys/" + crypto_key;
+    auto decrypt_response = client.Decrypt(crypto_key_name, encrypted_secret);
+    if (!decrypt_response) throw std::move(decrypt_response).status();
+    auto plaintext = decrypt_response->plaintext();
+    return plaintext;
+}
 
 int main(int argc, char *argv[]) {
 
-    test_kms();
+    /* test_kms();
 
-    test_secret();
+    test_secret(); */
+
+    auto encrypted_secret = get_encrypted_secret();
+    std::cout << "encrypted secret: " << encrypted_secret << "\n";
+
+    std::string decrypted_secret;
+
+    try {
+        decrypted_secret = decrypt_secret(encrypted_secret);
+    } catch (google::cloud::Status const& status) {
+        std::cerr << "google::cloud::Status thrown: " << status << "\n";
+    }
+
+    std::cout << "decrypted secret: " << decrypted_secret << "\n";
 
     CLI::App cli_app{"Lockbox Server"};
     cli_app.set_version_flag("--version", std::string("0.0.1"));
